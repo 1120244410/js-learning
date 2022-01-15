a0d10a8dd81338c83469c9ba50dde2777092cf96
韩嘉靖 <jj.han@neoclub.cn>
2022-01-15 14:59:49 +0800

    update: r

--- 埋点与监控/bp.js
+++ 埋点与监控/bp.js
@@ -1,38 +1,50 @@
-// 用来做点击埋点
-const bpCollect = function(event) {
-  if (!event) return;
-  nextNode(event.target);
-};
-function nextNode(node) {
-  const { attributes } = node;
-  if (attributes && attributes.hasOwnProperty("bp") && attributes.bp.value) {
-    bp(attributes.bp.value);
-  } else if (node.parentNode) {
-    nextNode(node.parentNode);
-  }
-}
-const bp = function(attr) {
-  console.log("埋点", attr);
-};
-document.addEventListener("click", bpCollect);
+import bridgeActions from "./appEvent";
 
-if (Vue) {
-  // 用来做pv、uv
-  Vue.directive("bp", {
-    // 绑定指令时 即dom生成
-    bind: function(el, { value, arg }) {
-      // 记一次pv
-      if (!arg || arg === "pv") {
-        bp(value);
-      }
-    },
-    // 解绑指令 即dom移除
-    unbind: function(el, { value, arg }) {
-      // 记一次uv
-      if (!arg || arg === "pl") {
-        bp(value);
-      }
+const init = function() {
+  // 用来做点击埋点
+  const bpCollect = function(event) {
+    if (!event) return;
+    nextNode(event.target);
+  };
+  function nextNode(node) {
+    const { attributes } = node;
+    if (
+      attributes &&
+      Object.prototype.hasOwnProperty.call(attributes, "bp") &&
+      attributes.bp.value
+    ) {
+      bp(attributes.bp.value);
+    } else if (node.parentNode) {
+      nextNode(node.parentNode);
     }
-    // 以上命令涉及dom创建与移除，需要搭配v-if或其他适合的操作
-  });
-}
+  }
+  const bp = function(attr) {
+    if (typeof attr === "string") {
+      attr = JSON.parse(attr);
+    }
+    bridgeActions.sendAppNewMaidian(attr);
+  };
+  document.addEventListener("click", bpCollect);
+  if (window.Vue) {
+    // 用来做pv、uv
+    window.Vue.directive("bp", {
+      // 绑定指令时 即dom生成
+      bind: function(el, { value, arg }) {
+        // 记一次pv
+        if (!arg || arg === "pv") {
+          bp(value);
+        }
+      },
+      // 解绑指令 即dom移除
+      unbind: function(el, { value, arg }) {
+        // 记一次uv
+        if (!arg || arg === "pl") {
+          bp(value);
+        }
+      }
+      // 以上命令涉及dom创建与移除，需要搭配v-if或其他适合的操作
+    });
+  }
+};
+
+export default init;
