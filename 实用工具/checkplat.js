af2d33fbee5a57ed70a07d164d745aaa990a0cdd
韩嘉靖 <jj.han@neoclub.cn>
2022-08-12 12:07:08 +0800

    update: r

--- 实用工具/checkplat.js
+++ 实用工具/checkplat.js
@@ -1,10 +1,6 @@
 export const checkPlatform = (function() {
   const u = navigator.userAgent;
-  if (
-    window.location.host !== "devukih5.neoclub.cn" &&
-    window.location.host !== "ukih5.neoclub.cn" &&
-    window.location.host !== "h5.hulaapp.cn"
-  ) {
+  if (window.location.host !== "") {
     return "web";
   }
   if (u.includes("Android") || u.includes("Adr")) {
